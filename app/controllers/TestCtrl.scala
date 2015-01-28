9f39f7a3b388d68815fb13a88df5636ba3bbb5b5
Steffen Haase <steffen.haase@epost-dev.de>
2015-01-28 10:48:26 +0100

    only for testing

---
+++ app/controllers/TestCtrl.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+object TestCtrl extends Controller {
+  def helloWorld = Action {
+    request => {
+      println(request.queryString)
+      Ok
+    }
+  }
+}
